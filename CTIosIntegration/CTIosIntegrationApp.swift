8e275095916137bd52bcf1c90af2971b92f14b4a
Harshitha <harshitha@clevertap.com>
2022-11-10 13:00:17 +0530

    Initial Commit

---
+++ CTIosIntegration/CTIosIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTIosIntegrationApp.swift
+//  CTIosIntegration
+//
+//  Created by Harshitha Maddina on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CTIosIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CTIosIntegration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CTIosIntegration
+//
+//  Created by Harshitha Maddina on 10/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
